3f2a9c4a829d6897975938fb8200f0fb06fd2a9f
xtophe38 <christophe.naud@gmail.com>
2023-06-14 18:01:31 +0200

    Update copyright

--- main.php
+++ main.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @copyright   Copyright (C) 2010-2023 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
 
 // Initialize collection plan
 require_once(APPROOT.'collectors/src/AnsibleCollectionPlan.class.inc.php');
