50d5f4343dcd83cd16293f89e5c78655ef305529
xtophe38 <christophe.naud@gmail.com>
2023-09-25 15:53:31 +0200

    Set first release revision

--- module.itop-data-collector-ansible.php
+++ module.itop-data-collector-ansible.php
@@ -6,7 +6,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-data-collector-ansible/1.0.0-dev',
+	'itop-data-collector-ansible/1.0.0',
 	array(
 		// Identification
 		//
